7548a2be63d0dbe591bb8179e488169e205d16e8
Lou Kielhorn <loukielhorn@gmail.com>
2023-12-12 17:21:26 +0100

    Added the text from the textarea to the messaging host and added that the response gets written back to the textarea.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     var changeTextButton = document.getElementById('changeTextButton');
+    var textarea = document.getElementById('text');
 
     changeTextButton.addEventListener('click', function() {
-        // Beispieltext
-        var textToProcess = "Hallo";
-        console.log("Sende Text: " + textToProcess);
 
-        // Verwende Native Messaging, um das Python-Skript zu starten
-        chrome.runtime.sendNativeMessage('ai_detector_fooler', { text: textToProcess }, function (response) {
-            if (chrome.runtime.lastError) {
-                console.log('Error:', chrome.runtime.lastError);
-            } else {
-                // Zeige die verarbeitete Antwort an
-                console.log(response.processed_text)
-            }
-        });
+        var text = textarea.value;
+        console.log("Send message via messaging host: " + text);
+
+        // Use messaging host to send the text to the python script
+        chrome.runtime.sendNativeMessage('ai_detector_fooler', { text: text }, onReceivedTextSend);
     });
 });
+
+function onReceivedTextSend(response) {
+    if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+    }
+    console.log("Response:", response);
+
+    try {
+        // If response is already an object, use it directly.
+        var jsonResponse = typeof response === 'object' ? response : JSON.parse(response);
+        var textarea = document.getElementById('text');
+        textarea.value = jsonResponse.response;
+    } catch (error) {
+        console.error("Error parsing JSON response:", error);
+    }
+}
